5759f113ea9f85f8d505fa4281bf8663e629923a
占明 <zhanming@syscloud.cn>
2019-01-31 15:45:33 +0800

    update

--- src/shared/account/login.service.ts
+++
@@ -1,8 +0,0 @@
-import fetch from "@/shared/api/request";
-
-export const LoginService = {
-    //登录获取验证码
-    getCodes: (params: any) => fetch('/account/api', params, 'POST'),
-    login: (params: any) => fetch('/account/api', params, 'POST'),
-
-};
